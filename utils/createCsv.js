8451f859040be450e6ef4864c5664010fff8fbd6
eladzlot <ez.implicit@gmail.com>
2016-11-24 22:25:01 +0200

    feat(format): flip csv

--- utils/createCsv.js
+++ utils/createCsv.js
@@ -1,29 +1,29 @@
 define(['./downloadFile.js', './UAparser.js'], function(downloadFile, UAParser){
     
     function createCsv(data){
+        var csv = [
+            'Browser,"Browser Version","Short Browser Version",Engine,"Engine Version",OS,"OS Version",Measurement,Latnecy' 
+        ];
         var parser = new UAParser();
         var ua = parser.getResult();
-        var csv = [];
+        var fileName = [ua.browser.name, ua.browser.version, ua.os.name, +new Date() + ''].map(snakeCase).join('-')+'.csv';
+        var browserData = [
+            ua.browser.name,
+            ua.browser.version,
+            /^[0-9]*/.exec(ua.browser.version)[0], // simplified browser version (only first number in string
+            ua.engine.name,
+            ua.engine.version,
+            ua.os.name,
+            ua.os.version
+        ].map(escapeCsv);
 
         for (var key in data) {
             var dataArr = data[key];
-            var fileName = [ua.browser.name, ua.browser.version, ua.os.name, +new Date() + ''].map(snakeCase).join('-')+'.csv';
-            var browserData = [
-                ua.browser.name,
-                ua.browser.version,
-                /^[0-9]*/.exec(ua.browser.version)[0], // simplified browser version (only first number in string
-                ua.engine.name,
-                ua.engine.version,
-                ua.os.name,
-                ua.os.version,
-                key
-            ].map(escapeCsv);
-
-            var csvContent = browserData.concat(dataArr).join(',');
-            csv.push(csvContent);
+            var csvArr = dataArr.map(function(value){ return browserData.concat(escapeCsv(key), value).join(','); });
+            csv = csv.concat(csvArr);
         }
 
-        downloadFile(fileName,csvContent); 
+        downloadFile(fileName,csv.join('\n')); 
     }
 
     return createCsv;
